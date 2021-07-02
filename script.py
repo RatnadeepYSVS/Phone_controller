b64fd61e2bee610d45ea5c117ee8d957d585373b
ratnadeep <ratnadeeppandu9@gmail.com>
2021-07-02 22:24:01 +0530

    short fixes

--- script.py
+++ script.py
@@ -26,8 +26,9 @@ def caller():
         accepted=False
         call_state=data[0].split('=')[-1]
         if phone_call:
-            speak(f'Incoming call from {phone_call[3:]}')
             incoming_call=phone_call
+            call_log.add(incoming_call+f' IN {datetime.now().hour}:{str(datetime.now().minute).zfill(2)}')
+            speak(f'Incoming call from {phone_call[3:]}')
             sleep(1)
         if not accepted and phone_call:
             missed_call_list.add(phone_call)
@@ -39,14 +40,13 @@ def caller():
                 pass
             else:
                 if incoming_call:
-                    call_log.add(incoming_call+f' IN {datetime.now().hour}:{str(datetime.now().minute).zfill(2)}')
                     incoming_call=''
         sleep(1)
 
 incoming_call_thread=Thread(target=caller)#Declaring the thread
 incoming_call_thread.start()#Starting the thread
 while True:
-    print('Options:-\n1 to call someone\n2.backup ur images to laptop\n3.get missed call log\n4.get call log\n5.lock ur phone')
+    print('Options:-\n1 to call someone\n2.backup ur images to laptop\n3.get missed call or calls u have cut stats\n4.get call log\n5.lock ur phone')
     k=int(input('enter option '))
     if k==1:
         phone_num=input('enter mobile number with country code ')
@@ -60,12 +60,14 @@ while True:
         cmd=f'adb pull /sdcard/DCIM/Camera {fol_path}'
         Popen(cmd,shell=True,stdin=PIPE,stdout=PIPE)
     elif k==3:
-        print(f'You have {len(missed_call_list)} missed call(s)')
         if len(missed_call_list):
+            print(f'Calls U ended up or missed')
             missed_call_table=PrettyTable(["Phone Numbers"])
             for i in list(missed_call_list):
                 missed_call_table.add_row([i])
             print(missed_call_table)
+        else:
+            print("You didn't miss or cut any call")
     elif k==4:
         print('Your call log from this laptop')
         if len(call_log):
